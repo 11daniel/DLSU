de997ef2bb1280b800932afae148cfcf60889846
Daniel Gavrie Clemente <69893252+11daniel@users.noreply.github.com>
2025-06-16 22:29:50 +0800

    Add files via upload

---
+++ CSOPESY/marquee/threaded.cpp
@@ -0,0 +1,83 @@
+#include <conio.h>
+#include <bits/stdc++.h>
+#include <unistd.h>
+
+//g++ -std=c++17 -o threaded threaded.cpp
+
+std::string currCommand;
+std::vector<std::string> commands;
+int x = 0;
+int y = 0;
+int sizeX = 100;
+int sizeY = 15;
+int yCount = 1;
+int xCount = 1;
+
+void pollKeyboard() {
+    while(true) {
+        if (_kbhit()) {
+            char key = _getch();
+            if (key==13) {
+                currCommand.push_back('\n');
+                commands.push_back(currCommand);
+                currCommand = "";
+            } else if (key==8) {
+                if (currCommand.length()>0) {
+                    currCommand.pop_back();
+                }
+            } else {
+                currCommand.push_back(key);
+            }
+        }
+        usleep(1000);
+    }
+}
+
+void drawScreen() {
+    while(true) {
+        system("cls");
+        std::cout << "*****************************************************\n"
+                << "*\tDisplaying a Marquee Console!\t*\n"
+                << "*****************************************************\n";
+
+        //set coords
+        if (y == 1 && yCount < 0) {
+            yCount = -yCount;
+        } else if (y==sizeY-1 && yCount > 0) {
+            yCount = -yCount;
+        }
+        y+=yCount;
+
+
+        if (x == 1 && xCount < 0) {
+            xCount = -xCount;
+        } else if (x==sizeX && xCount > 0) {
+            xCount = -xCount;
+        }
+        x+=xCount;
+
+        for(int i = 1;i<y;i++) {
+            std::cout << "\n";
+        }
+        std::cout << std::setw(23+x) << "Hello world in Marquee!";
+        for(int i = y;i<sizeY;i++) {
+            std::cout << "\n";
+        }
+
+        std::cout << "Enter a command for MARQUEE CONSOLE: " << currCommand << "\n";
+        std::reverse(commands.begin(), commands.end());
+        for(std::string command:commands) {
+            std::cout << "Command processed in MARQUEE_CONSOLE: " << command;
+        }
+        std::reverse(commands.begin(), commands.end());
+        usleep(5000);
+    }
+}
+
+int main() {
+    std::thread polling(pollKeyboard);
+    std::thread drawing(drawScreen);
+    polling.join();
+    drawing.join();
+    return 0;
+}
